f4e8ee8b57be0d684969c0cda8062609c4279b25
Robert Newton <newtorob@oregonstate.edu>
2017-06-18 16:45:39 -0400

    Updates

--- database/dbconf.js
+++ database/dbconf.js
@@ -6,12 +6,10 @@ This file is the controller for the server requests
 
 var mysql = require('mysql');
 var pool = mysql.createPool({
-  connectionLimit : 10,
-  host: 'mysql.cs.orst.edu',
-  user: 'cs290_newtorob',
-  password: '4180Stanky!',
-  database: 'student',
-  dateStrings:'true'
+  host  : 'localhost',
+  user  : 'student',
+  password: 'default',
+  database: 'student'
 });
 module.exports.pool = pool;
 
