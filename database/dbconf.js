95fa26e364d4d1322049d51f53e16cc54d82d8a6
Robert Newton <newtorob@oregonstate.edu>
2017-06-18 20:59:46 -0400

    changes

--- database/dbconf.js
+++ database/dbconf.js
@@ -3,27 +3,14 @@ Name: Robert Newton
 CS290 HW Assignment: Database Interactions
 This file is the controller for the server requests
 ***********************************************/
-/*
+
 var mysql = require('mysql');
 var pool = mysql.createPool({
   host  : 'mysql.cs.orst.edu',
   user  : 'cs290_newtorob',
   password: '4180Stanky!',
-  database: 'cs290_newtorob'
-});
-module.exports.pool = pool;
-
-*/
-
-
-var mysql = require('mysql');
-var pool = mysql.createPool({
-  connectionLimit: 10,
-  host:'localhost',
-  user:'student',
-  password:'default',
-  database:'student',
+  database: 'cs290_newtorob',
   dateStrings:'true'
 });
-
 module.exports.pool = pool;
+
