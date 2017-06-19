5c8593e51d431c9702dc0e7783d60cf5dccc5f53
Robert Newton <newtorob@oregonstate.edu>
2017-06-18 22:48:51 -0400

    updates

--- database/app.js
+++ database/app.js
@@ -29,7 +29,7 @@ var pool = mysql.createPool({
 
 app.get('/reset-table', function(req, res, next) {
     var context = {};
-    mysql.pool.query("DROP TABLE IF EXISTS workouts", function(err) { //replace your connection pool with the your variable containing the connection pool
+    pool("DROP TABLE IF EXISTS workouts", function(err) { //replace your connection pool with the your variable containing the connection pool
         var createString = "CREATE TABLE workouts(" +
             "id INT PRIMARY KEY AUTO_INCREMENT," +
             "name VARCHAR(255) NOT NULL," +
